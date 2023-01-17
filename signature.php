52a3d5d5183bc322a8b9b54bb47ca4ebc7002f46
loris-briguet <loris.briguet@ecal.ch>
2023-01-17 19:41:17 +0100

    Update signature.php

--- signature.php
+++ signature.php
@@ -1,23 +1,23 @@
 <?php
 
+
 // Array of emojis
-$emojis = [
- "😀", "😁", "😂", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😎", "😍",
- "😘", "😗", "😙", "😚", "🙂", "🤗", "🤔", "😐", "😑", "😶", "🙄", "😏",
- "😣", "😥", "😮", "😯", "😪", "😫", "😴", "😌", "😛", "😜", "😝", "🤤",
- "😒", "😓", "😔", "😕", "🙃", "🤑", "😲", "☹️", "🙁", "😖", "😞", "😟",
- "😤", "😢", "😭", "😦", "😧", "😨", "😩", "😬", "😰", "😱", "😳", "😵",
- "😡", "😠", "🤬", "😷", "🤒", "🤕", "🤢", "🤮", "🤧", "😇", "🤠", "🤡",
- "🤥", "🤫", "🤭", "🧐", "🤓", "😈", "👿", "👹", "👺", "💀", "☠️", "👻",
- "👽", "👾", "🤖", "💩", "😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿",
- "😾"
-];
+$emojis = [ "🥹", "😅", "🥲", "😊", "😇", "🙃", "😍", "🥰", "😘", "🤓", "😎", "🥸", "🤩", "🥳", "😞", "🥺", "😢", "😡", "🤬", "🤯", "😳", "🥵", "🥶", "😱", "🤗", "🫣",  "🤫", "🫠", "🤥", "😶", "🫥", "🙄",  "😴", "🤤","😵", "😵‍💫", "🤐", "🥴", "🤢", "🤮", "🤧", "😷", "🤕", "🤠", "🤡", "👽", "🎃", "💀", "👌", "👁", "🧠", "🧶", "💍", "🐙", "🌳", "🌸", "🌞", "🌝", "🌚", "🌍", "🍏", "🍑", "🍅", "🫒", "🍔", "🍤", "🍩", "🍪", "⚽️", "⚾️", "🏀", "🎾", "🎱", "🏵", "💿", "⏱", "⏰", "🪙", "⚙️", "🪩", "💝", "⭕️", "🚫", "🔞", "🚯", "🌐", "🌀", "🕐", ];
 
 // Generate a random signature with 3 emojis
-$signature = "<html><body><p>Best regards,</p>";
-for ($i = 0; $i < 3; $i++) {
- $signature .= "<span>" . $emojis[array_rand($emojis)] . "</span> ";
+for ($i = 0; $i < 1; $i++) {
+  $signature = "<html><body><h1 style='font-family: Helvetica; display: inline; font-size: 3em'>GIN</h1><span style='display: inline; font-size: 2.6em; margin-left: -0.05em; vertical-align: top'>" . $emojis[array_rand($emojis)] . "</span><h1 style='font-family: Helvetica; display: inline; font-size: 3em; margin-left: -0.05em;'>.</h1> ";
 }
-$signature .= "</body></html>";
+$signature .= "<p style='line-height: 20%;font-family: Helvetica; text-decoration: none; color: black'>Selma Mandoudi</p>
+      <p style='font-family: Helvetica; text-decoration: none; color: black; line-height: 90%'>
+        +41 76 298 11 73 <br />
+        <a
+          style='font-family: Helvetica; text-decoration: none !important; color: black !important'
+          href='mailto:booking.gino@lorisbriguet.ch'
+          target='_blank'
+        >
+          booking.gino@lorisbriguet.ch
+        </a>
+      </p></body></html>";
 
 echo $signature;
